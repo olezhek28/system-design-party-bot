5d9efef1ce21ed01331fe97418e8399338d5af64
Oleg Kozyrev <okozyrev@ozon.ru>
2023-01-02 19:38:31 +0600

    comment topic stats

--- internal/service/processor/get_topic_stats.go
+++ internal/service/processor/get_topic_stats.go
@@ -2,118 +2,150 @@ package processor
 
 import (
 	"context"
-	"database/sql"
-	"strings"
 
 	tgBotAPI "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/olezhek28/system-design-party-bot/internal/helper"
 	"github.com/olezhek28/system-design-party-bot/internal/model"
-	meetingRepository "github.com/olezhek28/system-design-party-bot/internal/repository/meeting"
-	"github.com/olezhek28/system-design-party-bot/internal/template"
-	"github.com/pkg/errors"
 )
 
 func (s *Service) GetTopicStats(ctx context.Context, msg *model.TelegramMessage) (tgBotAPI.MessageConfig, error) {
-	meets, err := s.meetingRepository.GetList(ctx, &meetingRepository.Query{
-		QueryFilter: model.QueryFilter{
-			AllData: true,
-		},
-		Status: sql.NullString{String: model.MeetingStatusFinished, Valid: true},
-	})
-	if err != nil {
-		return tgBotAPI.MessageConfig{}, err
-	}
+	//meets, err := s.meetingRepository.GetList(ctx, &meetingRepository.Query{
+	//	QueryFilter: model.QueryFilter{
+	//		AllData: true,
+	//	},
+	//	Status: sql.NullString{String: model.MeetingStatusFinished, Valid: true},
+	//})
+	//if err != nil {
+	//	return tgBotAPI.MessageConfig{}, err
+	//}
+	//
+	//// unitID[topicID][speakerID] = count
+	//// TODO сделать обертку для таких мап с интерфейсом добавления и со всякими проверками внутри
+	//stats := make(map[int64]map[int64]map[int64]int64)
+	//for _, meet := range meets {
+	//	if _, ok := stats[meet.UnitID]; !ok {
+	//		stats[meet.TopicID] = make(map[int64]map[int64]int64)
+	//	}
+	//	if _, ok := stats[meet.UnitID][meet.TopicID]; !ok {
+	//		stats[meet.UnitID][meet.TopicID] = make(map[int64]int64)
+	//	}
+	//
+	//	stats[meet.UnitID][meet.TopicID][meet.SpeakerID]++
+	//}
+	//
+	//topicMap, err := s.getTopicMap(ctx, meets)
+	//if err != nil {
+	//	return tgBotAPI.MessageConfig{}, err
+	//}
+	//
+	//speakerMap, err := s.getSpeakerMap(ctx, meets)
+	//if err != nil {
+	//	return tgBotAPI.MessageConfig{}, err
+	//}
+	//
+	//res := strings.Builder{}
+	//t, err := helper.ExecuteTemplate(template.TopicStatsIntroduction, nil)
+	//if err != nil {
+	//	return tgBotAPI.MessageConfig{}, err
+	//}
+	//
+	//res.WriteString(t + "\n")
+	//
+	//for unitID, unitStats := range stats {
+	//	for topicID, topicStats := range unitStats {
+	//		topic := topicMap[unitID][topicID]
+	//		t, err := helper.ExecuteTemplate(template.TopicStatsTopic, topic)
+	//		if err != nil {
+	//			return tgBotAPI.MessageConfig{}, err
+	//		}
+	//
+	//		res.WriteString(t + "\n")
+	//
+	//		for speakerID, count := range topicStats {
+	//
+	//			speaker := speakerMap[speakerID]
+	//			t, err := helper.ExecuteTemplate(template.TopicStatsSpeaker, map[string]interface{}{
+	//				"Speaker": speaker,
+	//				"Count":   count,
+	//			})
+	//			if err != nil {
+	//				return tgBotAPI.MessageConfig{}, err
+	//			}
+	//
+	//			res.WriteString(t + "\n")
+	//		}
+	//	}
+	//}
+	//
+	//for tID, topicStats := range stats {
+	//	topic, ok := topicMap[tID]
+	//	if !ok {
+	//		errors.Errorf("topic with id %d not found\n", tID)
+	//		continue
+	//	}
+	//
+	//	t, err = helper.ExecuteTemplate(template.TopicStatsHeader, struct {
+	//		TopicName string
+	//	}{
+	//		TopicName: topic.Name,
+	//	})
+	//	if err != nil {
+	//		return tgBotAPI.MessageConfig{}, err
+	//	}
+	//
+	//	res.WriteString(t + "\n")
+	//
+	//	for speakerID, count := range topicStats {
+	//		speaker, ok := speakerMap[speakerID]
+	//		if !ok {
+	//			errors.Errorf("speaker with id %d not found\n", speakerID)
+	//			continue
+	//		}
+	//
+	//		t, err = helper.ExecuteTemplate(template.TopicStats, struct {
+	//			FirstName        string
+	//			LastName         string
+	//			TelegramUsername string
+	//			Count            int64
+	//		}{
+	//			FirstName:        speaker.FirstName,
+	//			LastName:         speaker.LastName,
+	//			TelegramUsername: speaker.TelegramUsername,
+	//			Count:            count,
+	//		})
+	//		if err != nil {
+	//			return tgBotAPI.MessageConfig{}, err
+	//		}
+	//
+	//		res.WriteString(t + "\n")
+	//	}
+	//}
+	//
+	//reply := tgBotAPI.NewMessage(msg.From.ID, res.String())
+	//return reply, nil
 
-	// topicID[speakerID] = count
-	stats := make(map[int64]map[int64]int64)
-	for _, meet := range meets {
-		if _, ok := stats[meet.TopicID]; !ok {
-			stats[meet.TopicID] = make(map[int64]int64)
-		}
-
-		stats[meet.TopicID][meet.SpeakerID]++
-	}
-
-	topicMap, err := s.getTopicMap(ctx, meets)
-	if err != nil {
-		return tgBotAPI.MessageConfig{}, err
-	}
-
-	speakerMap, err := s.getSpeakerMap(ctx, meets)
-	if err != nil {
-		return tgBotAPI.MessageConfig{}, err
-	}
-
-	res := strings.Builder{}
-	t, err := helper.ExecuteTemplate(template.TopicStatsIntroduction, nil)
-	if err != nil {
-		return tgBotAPI.MessageConfig{}, err
-	}
-
-	res.WriteString(t + "\n")
-
-	for tID, topicStats := range stats {
-		topic, ok := topicMap[tID]
-		if !ok {
-			errors.Errorf("topic with id %d not found\n", tID)
-			continue
-		}
-
-		t, err = helper.ExecuteTemplate(template.TopicStatsHeader, struct {
-			TopicName string
-		}{
-			TopicName: topic.Name,
-		})
-		if err != nil {
-			return tgBotAPI.MessageConfig{}, err
-		}
-
-		res.WriteString(t + "\n")
-
-		for speakerID, count := range topicStats {
-			speaker, ok := speakerMap[speakerID]
-			if !ok {
-				errors.Errorf("speaker with id %d not found\n", speakerID)
-				continue
-			}
-
-			t, err = helper.ExecuteTemplate(template.TopicStats, struct {
-				FirstName        string
-				LastName         string
-				TelegramUsername string
-				Count            int64
-			}{
-				FirstName:        speaker.FirstName,
-				LastName:         speaker.LastName,
-				TelegramUsername: speaker.TelegramUsername,
-				Count:            count,
-			})
-			if err != nil {
-				return tgBotAPI.MessageConfig{}, err
-			}
-
-			res.WriteString(t + "\n")
-		}
-	}
-
-	reply := tgBotAPI.NewMessage(msg.From.ID, res.String())
-	return reply, nil
+	return tgBotAPI.MessageConfig{}, nil
 }
 
-func (s *Service) getTopicMap(ctx context.Context, meets []*model.Meeting) (map[int64]*model.Topic, error) {
+func (s *Service) getTopicMap(ctx context.Context, meets []*model.Meeting) (map[int64]map[int64]*model.Topic, error) {
 	var topicIds []int64
+	var unitIds []int64
 	for _, m := range meets {
 		topicIds = append(topicIds, m.TopicID)
+		unitIds = append(unitIds, m.UnitID)
 	}
 
-	topicsInfo, err := s.topicRepository.GetTopicsByIDs(ctx, topicIds)
+	topicsInfo, err := s.topicRepository.GetTopicsByIDs(ctx, unitIds, topicIds)
 	if err != nil {
 		return nil, err
 	}
 
-	topicMap := make(map[int64]*model.Topic)
+	topicMap := make(map[int64]map[int64]*model.Topic)
 	for _, topic := range topicsInfo {
-		topicMap[topic.ID] = topic
+		if _, ok := topicMap[topic.UnitID]; !ok {
+			topicMap[topic.UnitID] = make(map[int64]*model.Topic)
+		}
+
+		topicMap[topic.UnitID][topic.ID] = topic
 	}
 
 	return topicMap, nil
