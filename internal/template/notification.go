b592635a7b525b5c8dbd2c1cfba9fe0bee08cd34
Oleg Kozyrev <okozyrev@ozon.ru>
2023-02-06 12:57:27 +0600

    [repo-fix] fix mistake

--- internal/template/notification.go
+++ internal/template/notification.go
@@ -4,7 +4,7 @@ import (
 	"github.com/olezhek28/system-design-party-bot/internal/model"
 )
 
-const NotificationAfterCreate = `{{ .Emoji }} Пользователь <b>{{ .FirstName }} {{ .LastName }}</b> (@{{ .TelegramUsername }}) запланировал с тобой встречу по теме <b>"{{ .TopicName}}"</b> из раздела <b>"{{ .UnitName}}"</b> на <b>{{ .StartDate }}</b> (в твоём часов поясе).
+const NotificationAfterCreate = `{{ .Emoji }} Пользователь <b>{{ .FirstName }} {{ .LastName }}</b> (@{{ .TelegramUsername }}) запланировал с тобой встречу по теме <b>"{{ .TopicName}}"</b> из раздела <b>"{{ .UnitName}}"</b> на <b>{{ .StartDate }}</b> (в твоём часовом поясе).
 `
 
 const NotificationBeforeStart = `{{ .Emoji }} Йоу! Напоминаю, что через <b>` + model.ReminderTime + `</b> минут у тебя запланирована встреча с пользователем <b>{{ .FirstName }} {{ .LastName }}</b> (@{{ .TelegramUsername }}) по теме <b>"{{ .TopicName}}"</b> из раздела <b>"{{ .UnitName}}"</b>. Не забудь!
